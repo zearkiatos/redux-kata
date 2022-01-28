32bd9a9b59df4d46c51b3076912bae1c42bcbbdd
zearkiatos <caprilespe@outlook.com>
2022-01-28 18:00:18 -0300

    Make action creators

--- src/boilerplate.js
+++ src/boilerplate.js
@@ -1,9 +1,23 @@
 const makeType = (module) => (type) => `${module}/${type}`;
 
-const type = makeType('LIST');
+const type = makeType("LIST");
 
-const ADD_TODO = type('ADD_TODO');
-const REMOVE_TODO = type('REMOVE_TODO');
-const UPDATE_TODO = type('UPDATE_TODO');
+const makeActionCreator =
+  (type, ...argumentsName) =>
+  (...arguments) => {
+    const action = { type };
+    argumentsName.forEach((argument, index) => {
+      action[argumentsName[index]] = arguments[index];
+    });
+    return action;
+  };
 
-const ADD_TODO = "FORM/ADD_TODO";
+const ADD_TODO = type("ADD_TODO");
+const REMOVE_TODO = type("REMOVE_TODO");
+const UPDATE_TODO = type("UPDATE_TODO");
+
+const addTodo = makeActionCreator(ADD_TODO, "payload");
+const removeTodo = makeActionCreator(REMOVE_TODO, "payload");
+const updateTodo = makeActionCreator(UPDATE_TODO, "payload");
+
+console.log(addTodo(1));
