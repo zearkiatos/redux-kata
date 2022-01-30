c9640eea317b5170dc8ef1e2a9bad51124475363
zearkiatos <caprilespe@outlook.com>
2022-01-30 08:49:54 -0300

    Function reducer optimization

--- src/boilerplate.js
+++ src/boilerplate.js
@@ -12,11 +12,18 @@ const makeActionCreator =
     return action;
   };
 
-const type = makeType('TODOS');
+const createReducer =
+  (initializationState, handlers) =>
+  (state = initializationState, action) =>
+    handlers.hasOwnProperty(action.type)
+      ? handlers[action.type](type, action)
+      : state;
 
-const FETCH_START = type('FETCH_START');
-const FETCH_SUCCESS = type('FETCH_SUCCESS');
-const FETCH_ERROR = type('FETCH_ERROR');
+const type = makeType("TODOS");
+
+const FETCH_START = type("FETCH_START");
+const FETCH_SUCCESS = type("FETCH_SUCCESS");
+const FETCH_ERROR = type("FETCH_ERROR");
 
 const initialState = {
   data: [],
@@ -24,37 +31,30 @@ const initialState = {
   fetching: false
 };
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case FETCH_START: {
-      return {
-        ...state,
-        fetching: true
-      };
-    }
-    case FETCH_SUCCESS: {
-      return {
-        ...state,
-        fetching: false,
-        fetched: true,
-        data: action.payload
-      };
-    }
-    case FETCH_ERROR: {
-      return {
-        ...state,
-        fetching: false,
-        error: action.error
-      };
-    }
-  }
-};
+const fetchStartReducer = (state) => ({ ...state, fetching: true });
+const fetchSuccessReducer = (state, action) => ({
+  ...state,
+  fetching: false,
+  fetched: true,
+  data: action.payload
+});
+const fetchErrorReducer = (state, action) => ({
+  ...state,
+  fetching: false,
+  error: action.error
+});
+
+const reducer = createReducer(initialState, {
+  [FETCH_START]: fetchStartReducer,
+  [FETCH_SUCCESS]: fetchSuccessReducer,
+  [FETCH_ERROR]: fetchErrorReducer
+});
 
 const startFetch = makeActionCreator(FETCH_START);
 
-const successFetch = makeActionCreator(FETCH_SUCCESS, 'payload');
+const successFetch = makeActionCreator(FETCH_SUCCESS, "payload");
 
-const errorFetch = makeActionCreator(FETCH_ERROR, 'error');
+const errorFetch = makeActionCreator(FETCH_ERROR, "error");
 
 const fetch = () => {
   async (dispatch) => {
